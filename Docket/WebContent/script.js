df3502096e5457bbbd50b9e9bcd31059db97196e
Kevin Truong <truong.kevin.81824@gmail.com>
2021-12-01 19:36:46 -0800

    Revert "connected to db"
    This reverts commit 2fcd9613a986da86f06804dd3a889353c8f58954.

--- Docket/WebContent/script.js
+++ Docket/WebContent/script.js
@@ -1,59 +1,97 @@
-jQuery(document).ready(function () {
-  jQuery(".datetimepicker").datepicker({
-    timepicker: true,
-    language: "en",
-    range: true,
-    multipleDates: true,
-    multipleDatesSeparator: " - ",
-  });
-  jQuery("#add-event").submit(function () {
-    alert("Submitted");
+jQuery(document).ready(function(){
+    jQuery('.datetimepicker').datepicker({
+        timepicker: true,
+        language: 'en',
+        range: true,
+        multipleDates: true,
+            multipleDatesSeparator: " - "
+      });
+    jQuery("#add-event").submit(function(){
+        alert("Submitted");
 
-    var values = {};
-    $.each($("#add-event").serializeArray(), function (i, field) {
-      values[field.name] = field.value;
+        var values = {};
+        $.each($('#add-event').serializeArray(), function(i, field) {
+            values[field.name] = field.value;
+        });
+        console.log(
+          values
+        );
     });
-    console.log(values);
   });
-});
 
-(function () {
-  "use strict"; 
-  jQuery(function () {
+jQuery('#calendar').fullCalendar({
+      themeSystem: 'bootstrap4',
+      businessHours: false,
+      defaultView: 'month',
+      editable: true,
+      customButtons: {
+    	Add: {
+	      text: 'Add',
+	      click: function() {
+	    	  jQuery('#modal-view-event-add').modal();
+	      }
+	    }
+      },
+      header: {
+          left: 'title Add',
+          center: 'month,agendaWeek,agendaDay',
+          right: 'today prev,next'
+      },
+      //event sources 
+      //getAll from db
+    //   events: function(){
+    //         fetch('http://localhost:3000/getAll')
+    //         .then(response => response.json());                    
+    //   },
     
-    axios.get("http://localhost:3000/getall").then(results=>{
-      console.log("Events: ",results);
+      events: [
 
-      jQuery("#calendar").fullCalendar({
-        themeSystem: "bootstrap4",
-        businessHours: false,
-        defaultView: "month",
-        editable: true,
-        header: {
-          left: "title",
-          center: "month,agendaWeek,agendaDay",
-          right: "today prev,next",
+        {
+            title: 'Team Meeting',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu pellentesque nibh. In nisl nulla, convallis ac nulla eget, pellentesque pellentesque magna.',
+            start: '2021-11-10T13:00:00',
+            end: '2021-11-10T16:00:00',
+            className: 'fc-bg-pinkred',
+            icon : "group",
+            allDay: false
         },
-        events: results.data,
-        eventRender: function (event, element) {
-          if (event.icon) {
-            element
-              .find(".fc-title")
-              .prepend("<i class='fa fa-" + event.icon + "'></i>");
+        {
+            title: 'Team Meeting 2',
+            description: 'Discussion',
+            start: '2021-11-15T13:00:00',
+            end: '2021-11-15T16:00:00',
+            className: 'fc-bg-blue',
+            icon : "group",
+        },
+        {
+            title: 'Team Meeting 3',
+            description: 'Discussion',
+            start: '2021-11-15T13:00:00',
+            end: '2021-11-15T16:00:00',
+            className: 'fc-bg-blue',
+            icon : "group",
+        }
+    ],
+    
+      eventRender: function(event, element) {
+          if(event.icon){
+              element.find(".fc-title").prepend("<i class='fa fa-"+event.icon+"'></i>");
           }
         },
-        dayClick: function () {
-          jQuery("#modal-view-event-add").modal();
-        },
-        eventClick: function (event, jsEvent, view) {
-          jQuery(".event-icon").html("<i class='fa fa-" + event.icon + "'></i>");
-          jQuery(".event-title").html(event.title);
-          jQuery(".event-body").html(event.description);
-          jQuery(".eventUrl").attr("href", event.url);
-          jQuery("#modal-view-event").modal();
-        },
-      });
-    }).catch(err=>console.error);
-    
-  });
-})(jQuery);
+      eventClick: function(event, jsEvent, view) {
+          jQuery('.event-icon').html("<i class='fa fa-"+event.icon+"'></i>");
+          jQuery('.event-title').html(event.title);
+          jQuery('.event-start').html(event.start.format("YYYY-MM-DD").toString());
+          jQuery('.event-end').html(event.end.format("YYYY-MM-DD").toString());
+          jQuery('.event-id').html(event.id);
+          jQuery('.event-body').html(event.description);
+          jQuery('.eventUrl').attr('href',event.url);
+          jQuery('#modal-view-event').modal();
+          
+          document.getElementById("x-id").value=jQuery("#event-id").text();
+          document.getElementById("x-start").value=event.start.format("YYYY-MM-DD").toString();
+          document.getElementById("x-end").value=event.end.format("YYYY-MM-DD").toString();
+          console.log(event.start.toDate().toString());
+      }
+});
+
